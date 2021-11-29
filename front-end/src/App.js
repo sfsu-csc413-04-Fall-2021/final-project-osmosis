88a9f1a6f86046f9f8c12b75a0d85feb670718dd
RuweydaM <rmohamud@mail.sfsu.edu>
2021-11-28 20:29:40 -0800

    some simple pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import React from 'react';
+import { Link, Switch, Route} from 'react-router-dom';
+
+import Login from './Login';
+import Logout from './Logout';
 
 function App() {
+
+  
+
   return (
-    <div>
-      Add your code here!
+    <div >
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+          <Link to="/Logout">Logout</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path ="/Login">  <Login />  </Route>
+        <Route path ="/Logout">  <Logout />  </Route>
+      </Switch>
+
     </div>
+      
+
+     
+      
+  
   );
 }
 
---
+++ front-end/src/Login.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Login.css";
+
+
+function Login() {
+
+    const [user,setUser]= React.useState('');
+    const [password, setPassword]=React.useState('');
+  
+  const myHandler= () => {
+  
+    console.log( 'Username= '+ user);
+    console.log( 'Password= ' +password);
+    const body={
+        //body of post request
+        username: user,
+        password: password
+    };
+    const settings = {
+        method: 'post',
+        body: JSON.stringify(body),
+    };
+    fetch('/api/Login', settings)
+  
+  
+  };
+  
+  
+  
+    return (
+      <div className="login-box" >
+  
+        <div className="log-in">
+          <div className="contents">
+          <h2>Please Login</h2>
+          <div className="username">
+            <label>Username</label>
+            <br>
+            </br>  
+            <input value={user} onChange={(e) => setUser(e.target.value)} className="userName-input">
+  
+            </input>
+            <br>
+            </br>
+            <label>Password</label>
+            <br></br>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="userName-input">
+  
+            </input>
+            <br>
+            </br>
+            
+  
+      <button onClick={myHandler}>Submit</button>
+          </div>
+          </div>
+          
+          
+    
+  </div>
+        
+      </div>
+        
+  
+       
+        
+    
+    );
+  }
+
+export default Login;
