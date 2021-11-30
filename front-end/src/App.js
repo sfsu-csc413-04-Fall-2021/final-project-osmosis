5d2618ea2fc5ce35ba9c3a5e979bc896d925bdfe
danieljchang <danielchang0000@gmail.com>
2021-11-29 17:09:06 -0800

    Worked a little on the headers, turned it into a grid. Can always change the way it looks to being on top or being a side navbar.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,21 +5,29 @@ import { Link, Switch, Route} from 'react-router-dom';
 import Login from './Login';
 import Logout from './Logout';
 
-function App() {
-
-  
+function App() {  
 
   return (
     <div >
       <nav>
-        <ul>
-          <li>
+        <div id="main">
+          <div id="home" className="headers">
+            <Link to="/">Home</Link>
+          </div>
+          <div id="login" className="headers">
             <Link to="/Login">Login</Link>
-          </li>
-          <li>
-          <Link to="/Logout">Logout</Link>
-          </li>
-        </ul>
+          </div>      
+          <div id="logout" className="headers">
+            <Link to="/Logout">Logout</Link>
+          </div>          
+          <div id="tran" className="headers">
+            <Link to="/Transactions">Transactions</Link>
+          </div>
+          <div id="payment" className="headers">
+            <Link to= "/MakePayment">Make a Payment</Link>
+          </div>
+        </div>
+        
       </nav>
 
       <Switch>
