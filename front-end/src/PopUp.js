f0783b82a669cffe2e9f8ca1b01fb10b20b664fd
jsrion <jsrion@gmail.com>
2021-12-06 20:53:28 -0800

    finished ad

--- front-end/src/PopUp.js
+++ front-end/src/PopUp.js
@@ -10,16 +10,21 @@ function PopUp() {
             targetDiv.style.display = "block";
           }
     }
+    const signUp = () => {
+        window.location.replace("./SignUp")
+    }
     return (
         <div id="Popup" className="Popup">
             <div className="contents">
+                <br></br>
+                <br></br>
                 <h2>Attention!</h2>
-                <h4>Sign Up With Osmosis Today and Get a Free $1000</h4>
-                <br></br>
-                <br></br>
+                <h4>Sign Up With Osmosis Today and Get a Free</h4>
+                <h3>$1000</h3>
+                <h4>No Games, No Gimmocks!</h4>
                 <h6>For Verified Members Only</h6>
-                <br></br>
                 <button className="button1" onClick={closeAd}>Close</button>
+                <button className="button1" onClick={signUp}>Sign Up Now!</button>
             </div>
         </div>
     );
