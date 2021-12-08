4b347bbdbe2242a752b90b38697bdf0ebd82cd51
Devon Dy-Liacco <dreydee33@gmail.com>
2021-12-08 14:59:34 -0800

    Load transactions

--- front-end/src/Transactions.js
+++ front-end/src/Transactions.js
@@ -4,24 +4,99 @@ import "./Login.js"
 import "./Transactions.css";
 import { Link, Route, Switch } from 'react-router-dom';
 import Login from "./Login.js";
+import PopUp from "./PopUp.js";
 
 function Transaction(props) {
     return (
         <div className="box">
             <div className="sender">
-                {props.config.sender}
+                {props.sender}
             </div>
             <div className="recipient">
-                {props.config.recipient}
+                {props.recipient}
             </div>
             <div className="amount">
-                {props.config.amount}
+                {props.amount}
             </div>
         </div>
     );
 }
 
-function Transactions() {
+class TransactionsList extends React.Component {
+    render() {
+        console.log("props" + this.props.data.transactions.length);
+        if (this.props.data.transactions) {
+            console.log(this.props.data.transactions);
+            var transactions = this.props.data.transactions.map(
+                function (transaction) {
+                    console.log(transaction);
+                    return Transaction(transaction);
+                });
+            return (
+                <div className="transactions">
+                    {transactions}
+                </div>
+            );
+        }
+    }
+};
+
+class Transactions extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null
+        };
+    }
+
+    componentWillMount() {
+        this.renderMyData();
+    }
+
+    renderMyData() {
+        const body = {
+            username: Cookies.get("loggedIn")
+        };
+        const settings = {
+            method: 'post',
+            body: JSON.stringify(body)
+        };
+        fetch('api/view-all', settings)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({ data: responseJson })
+                console.log(responseJson);
+            })
+            .catch((error) => {
+                console.log("ERROR");
+                console.error(error);
+            });
+    }
+
+    render() {
+        const closeBox = () => {
+            window.location.replace("../") //return to home page if clicked
+        }
+        return (
+            <div className="transactionsbox">
+                <div className="contents">
+                    <h2>Transactions</h2>
+                    <label>Username: </label>
+                    {Cookies.get("loggedIn")}
+                    <br />
+                    {this.state.data ? <TransactionsList data={this.state.data} /> : <PopUp />}
+                    <button onClick={closeBox}>Close</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Transactions;
+
+/* function Transactions() {
     const [username, checkUsername] = React.useState('');
     const [balance, checkBalance] = React.useState('');
     const [history, checkHistory] = React.useState('');
@@ -51,7 +126,6 @@ function Transactions() {
 
     class TransactionsList extends React.Component {
         render() {
-            var array = this.props.PromiseResult
             console.log("props"+this.props.transactions.length);
             if (this.props.transactions) {
                 console.log(this.props.transactions);
@@ -81,9 +155,8 @@ function Transactions() {
             }
         }
     };
-    
+
     function loadTransaction(username) {
-        var transactionsList = [];
         const body = {
             username: username
         };
@@ -94,17 +167,16 @@ function Transactions() {
         console.log("fetch");
         fetch('api/view-all', settings)
             .then((data) => data.json())
-            .then((result => {
-                console.log("result");
-                console.log(result);
-                for(var i = 0;i < result.transactions.length;i++) {
-                    transactionsList.push(result.transactions.at(i));
-                }
-                return result;
-            }));
-        console.log("full list");
-        console.log(transactionsList);
-        return transactionsList;
+            .then()
+            // .then((result => {
+            //     console.log("result");
+            //     console.log(result);
+            //     for(var i = 0;i < result.transactions.length;i++) {
+            //         console.log(result.transactions.at(i));
+            //         transactionsList.push(result.transactions.at(i));
+            //     }
+            //     return result;
+            // }));
     }
 
     return (
@@ -116,14 +188,13 @@ function Transactions() {
 
                 <TransactionsList transactions={loadTransaction(Cookies.get("loggedIn"))} />
                 <br></br>
+                {this.state.data ? <TransactionsList transactions={this.state.data} /> : <TransactionsList /> }
                 <button onClick={closeBox}>Close</button>
             </div>
         </div>
 
     );
-}
-
-export default Transactions;
+} */
 /*<div className="form">
                 <div className="contents">
                     <h2>Transactions</h2>
